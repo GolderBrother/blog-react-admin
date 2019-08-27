70ccee894c57cc66ddd89d47a50fe12744925005
GolderBrother <1204788939@qq.com>
2019-08-27 21:30:13 +0800

    change request proxy

--- config/config.js
+++ config/config.js
@@ -61,7 +61,7 @@ export default {
   },
   proxy: {
     '/api': {
-      target: 'http://127.0.0.1:5000',
+      target: 'http://127.0.0.1:6000',
       changeOrigin: true,
       pathRewrite: { '^/api': '' },
     },
